32084e344dbd217b16f26274f03bbe427ed79e3d
yogesh-85 <yogeshyogi3568@gmail.com>
2024-01-23 22:15:04 +0530

    task-1 javascript

---
+++ task1/index.js
@@ -0,0 +1,12 @@
+const header1 = document.getElementById("main-heading");
+header1.textContent = 'Fruit World';
+header1.style.color = 'orange';
+const header2 = document.getElementById("header");
+header2.style.backgroundColor = 'green';
+header2.style.borderBottom = '3px solid orange';
+
+const basket = document.getElementById('basket-heading');
+basket.style.color = 'green';
+
+const thank = document.getElementById('thanks');
+thank.innerHTML = '<p>Please visit us again</p>';
